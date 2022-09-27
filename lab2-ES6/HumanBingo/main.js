27ef1c4768081ba0323ed4c8c569222b6966c2f9
jeffasseur <r0846107@student.thomasmore.be>
2022-09-27 14:15:36 +0200

    feat: setup files from codesandbox

--- lab2-ES6/HumanBingo/main.js
+++ lab2-ES6/HumanBingo/main.js
@@ -1,23 +1,5 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+// import another class
+import Bingo from "./js/bingo.js";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+// kickstart the app by calling the constructor via the `new` keyword
+const bingo = new Bingo();
