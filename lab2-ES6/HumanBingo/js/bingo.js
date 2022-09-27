89739ba6940ad4a1261045228bf8bc5c6eb59228
jeffasseur <r0846107@student.thomasmore.be>
2022-09-27 14:18:52 +0200

    feat: export class to use class elswhere

--- lab2-ES6/HumanBingo/js/bingo.js
+++ lab2-ES6/HumanBingo/js/bingo.js
@@ -1,7 +1,7 @@
-import Card from "./card.js";
+import Card from "./card";
 
 // 🔥🔥🔥 TODO 1 - make sure to export the class, if you want to be able to import the class elsewhere
-class Bingo {
+export default class Bingo {
   constructor() {
     // the constructor is called when you create a new instance of the class
     console.log("Welcome to Bingo! 🎉");
