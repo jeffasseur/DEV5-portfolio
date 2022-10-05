76764605d08166d2094a9d9dc67d5baa63f01a08
jeffasseur <r0846107@student.thomasmore.be>
2022-10-05 15:56:41 +0200

    feat: add weather to local storageand check if it exists

--- lab3-API/classes/Weather.js
+++ lab3-API/classes/Weather.js
@@ -29,12 +29,22 @@ export default class Weather {
     getWeather() {
         const url = `http://api.weatherapi.com/v1/current.json?key=${this.API_KEY}&q=${this.lat},${this.lng}&aqi=no`;
 
-        fetch(url)
-            .then(res => res.json())
-            .then(data => {
-                //console.log(data);
-                this.displayWeather(data);
-            });
+        // check if location exists in local storage
+        if (localStorage.getItem('weather') === null) {
+            // get weather from API
+            fetch(url)
+                .then(res => res.json())
+                .then(data => {
+                    //console.log(data);
+                    // store weather in local storage
+                    localStorage.setItem('weather', JSON.stringify(data));
+                    this.displayWeather(data);
+                });
+        } else {
+            // get weather from local storage
+            const weather = JSON.parse(localStorage.getItem('weather'));
+            this.displayWeather(weather);
+        }
     }
 
     displayWeather(data) {
