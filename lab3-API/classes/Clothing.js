311c57b614b0932127dd406b4d15a2cc376f322a
jeffasseur <r0846107@student.thomasmore.be>
2022-10-06 22:01:15 +0200

    feat: call Clothing class from Weather class and display clothing

--- lab3-API/classes/Clothing.js
+++ lab3-API/classes/Clothing.js
@@ -1,19 +1,51 @@
 import Weather from './Weather.js';
 
 export default class Clothing {
-    constructor(apiKey) {
+    constructor(apiKey, weather) {
         this.apiKey = apiKey;
         this.country = 'us';
         this.lang = 'en';
         this.currentPage = 0;
         this.pageSize = 30;
         this.categorie = 'sportswear';
-        this.functions = ['breatable', 'waterproof', 'windproof', 'thermal', 'quick dry', 'reflective details']; // &functions=${this.functions}
+        this.functions = "";
+        this.getWearables(weather);
     }
 
     getWearables(weather) {
-        this.functions = 'waterproof';
-        console.log("This from the Clothing class: " + weather);
+        switch (weather) {
+            case 'Sunny' || 'Clear':
+                this.functions = 'Breathable';
+                break;
+            
+            case 'Partly cloudy':
+                this.functions = 'Quickdry';
+                break;
+
+            case 'Cloudy':
+                this.functions = 'Reflectivedetails';
+                break;
+
+            case 'Light rain':
+                this.functions = 'Waterproof';
+                break;
+
+            case 'Moderate rain':
+                this.functions = 'Water-repellent';
+                break;
+
+            case 'Heavy rain':
+                this.functions = 'Water-repellent';
+                break;
+
+            case 'Overcast':
+                this.functions = 'Reflective%20details';
+                break;
+
+            default:
+                this.functions = 'Thermal';
+                break;
+        }
 
         const url = `https://apidojo-hm-hennes-mauritz-v1.p.rapidapi.com/products/list?country=${this.country}&lang=${this.lang}&currentpage=${this.currentPage}&pagesize=${this.pageSize}&categories=${this.categorie}&functions=${this.functions}`;
 
@@ -27,6 +59,10 @@ export default class Clothing {
         .then(res => res.json())
         .then(data => {
             console.log(data);
+            const img = document.querySelector('#clothingPiece');
+            const buyLink = document.querySelector('.clothing__buy');
+            img.src = data.results[0].images[0].url;
+            buyLink.href = data.baseUrl + data.results[0].linkPdp;
         })
         .catch(err => {
             console.error(err);
--- lab3-API/classes/Weather.js
+++ lab3-API/classes/Weather.js
@@ -1,3 +1,5 @@
+import Clothing from './Clothing.js';
+
 export default class Weather {
     constructor(API_KEY) {
         this.API_KEY = API_KEY;
@@ -31,24 +33,24 @@ export default class Weather {
         const url = `http://api.weatherapi.com/v1/current.json?key=${this.API_KEY}&q=${this.lat},${this.lng}&aqi=no`;
 
         // check if location exists in local storage
-        if (localStorage.getItem('weather') === null) {
+        if (localStorage.getItem('weather') && Date.now() - localStorage.getItem('weatherTime') < 1000 * 60 * 60) {
+            // get weather from local storage
+            const w = JSON.parse(localStorage.getItem('weather'));
+            this.displayWeather(w);
+        } else {
             // get weather from API
             fetch(url)
                 .then(res => res.json())
                 .then(data => {
-                    //console.log(data);
+                    console.log(data);
                     // store weather in local storage
                     localStorage.setItem('weather', JSON.stringify(data));
                     this.displayWeather(data);
                 });
-        } else {
-            // get weather from local storage
-            const weather = JSON.parse(localStorage.getItem('weather'));
-            this.displayWeather(weather);
         }
     }
 
-    async displayWeather(data) {
+    displayWeather(data) {
         const title = document.querySelector('.weather__condition');
         const icon = document.querySelector('.weather__icon');
 
@@ -56,6 +58,14 @@ export default class Weather {
         icon.src = data.current.condition.icon;
 
         this.weather = data.current.condition.text;
+        console.log("Display weather function " + this.weather);
+
+        this.getClothes(this.weather);
+    }
+
+    getClothes(weather) {
+        const wearables = new Clothing('446a5cb6f1mshe3be4fa8b0625e4p1ad412jsnca2e20050552', weather);
+        console.log("Get Clothes function inside Weather class: " + weather);
     }
 
 }
