f24cb21e8cea33fb7eba84b8c39eeed173fa0a28
jeffasseur <r0846107@student.thomasmore.be>
2022-10-06 22:03:29 +0200

    feat: try to call classes from main js file - didn't work

--- lab3-API/main.js
+++ lab3-API/main.js
@@ -1,4 +1,11 @@
 import Weather from './classes/Weather.js';
+import Clothing from './classes/Clothing.js';
 import './style.css'
 
 const App = new Weather('dcd54b8118d14a0396863452220510');
+//const wearables = new Clothing('446a5cb6f1mshe3be4fa8b0625e4p1ad412jsnca2e20050552', App.weather);
+
+const w = App.weather;
+console.log(w);
+
+//wearables.getWearables(w);
