a0513852d6bd73b2107768204968b416145b9185
jeffasseur <r0846107@student.thomasmore.be>
2022-10-12 15:06:49 +0200

    feat: add scene, camera and renderer

--- lab4-ThreeJS/main.js
+++ lab4-ThreeJS/main.js
@@ -1,1 +1,9 @@
 import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement)
