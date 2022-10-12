48ac335bddf9573b84e30ec329c6e33da542afb8
jeffasseur <r0846107@student.thomasmore.be>
2022-10-12 12:31:17 +0200

    feat: add orbitContols

--- lab4-ThreeJS/main.js
+++ lab4-ThreeJS/main.js
@@ -3,15 +3,17 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 55, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 55, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const controls = new OrbitControls( camera, renderer.domElement );
+
+
 // make a simple house
-const geometry = new THREE.PlaneGeometry( 1, 1);
+const geometry = new THREE.PlaneGeometry( 10, 10);
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
@@ -27,6 +29,8 @@ camera.position.z = 5;
 
 function animate() {
   requestAnimationFrame( animate );
+
+  controls.update();
 
   // cube.rotation.x += 0.007;
   // cube.rotation.y += 0.007;
