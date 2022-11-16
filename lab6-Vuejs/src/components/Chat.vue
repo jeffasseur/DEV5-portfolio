d84bcaa8077d97a36cb1908dfdfd6619f6c78eb0
JEF .FASSEUR <91196774+jeffasseur@users.noreply.github.com>
2022-11-16 15:08:42 +0100

    feat: fetch messages and display on screen and add own message

--- lab6-Vuejs/src/components/Chat.vue
+++ lab6-Vuejs/src/components/Chat.vue
@@ -1,12 +1,51 @@
-<script>
+<script setup>
+import { onMounted, reactive, ref } from 'vue'
+
+let comments = reactive( { comments: [] } )
+let message = ref('')
+
+onMounted( () => {
+    fetch('https://lab5-p379.onrender.com/api/v1/messages')
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            comments.comments = data
+        })
+})
+
+const addMessage = () => {
+    fetch('https://lab5-p379.onrender.com/api/v1/messages', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            user: 'Jef',
+            text: message.value
+        })
+    }) 
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        comments.comments.push({
+            _id: data.data._id,
+            user: data.data.user,
+            text: data.data.text
+        })
+    })
+}
 </script>
 
 <template>
     <div class="chat">
-        <div class="chat__single">
-            <h4>Username</h4>
-            <p>Description</p>
+        <div v-for="m in comments.comments" :key="m._id" class="chat__single">
+            <h4>{{m.user}}</h4>
+            <p>{{m.text}}</p>
         </div>
+    </div>
+    <div class="input">
+        <input type="text" name="text" id="text" v-model="message">
+        <button @click.prevent="addMessage">Verzenden</button>
     </div>
 </template>
 
@@ -18,5 +57,18 @@
         background: lightgray;
         padding: .2em 1em;
         border-radius: .5em;
+        height: 80vh;
+        overflow-y: scroll;
+    }
+    .chat__single {
+        padding: .5em;
+        background: gray;
+        color: #fff;
+    }
+    .chat__single h4 {
+        color: blue;
+    }
+    .input {
+        padding: 1em 0;
     }
 </style>
